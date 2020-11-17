d2cb6461986b131dbdc8162a9c6b0ff588f0303d
abrelm <abrelm@kth.se>
2020-11-17 21:15:14 +0100

    Initial Commit

---
+++ NineMenMorris/NineMenMorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  NineMenMorrisApp.swift
+//  NineMenMorris
+//
+//  Created by Abraham Elmgren on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct NineMenMorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
